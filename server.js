67084618c652ed68153b7550cd7ec40b418270c5
Gilisho123 <leteipagilisho@gmail.com>
2025-10-13 22:51:02 +0300

    Update API endpoints to live backend

--- server.js
+++ server.js
@@ -13,8 +13,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-import cors from "cors";
+// Middleware 
 
 app.use(cors({
   origin: "https://giltech-frontend.vercel.app",
